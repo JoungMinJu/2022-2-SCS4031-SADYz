0ae0a8d24cd9bd6af1e9cd83ab22ae8a21a1fd66
이건회 <rg970604@naver.com>
2022-10-20 00:17:56 +0900

    refactor : LastMovedTime.java

--- backend/src/main/java/SADYz/backend/client/domain/LastMovedTime.java
+++ backend/src/main/java/SADYz/backend/client/domain/LastMovedTime.java
@@ -1,6 +1,9 @@
 package SADYz.backend.client.domain;
 
 import SADYz.backend.client.domain.Client;
+import SADYz.backend.client.dto.LastMovedTimeDto;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 @Entity
 @NoArgsConstructor
@@ -17,7 +21,9 @@ public class LastMovedTime {
   @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private LocalDateTime lastMovedTime = LocalDateTime.parse("2022-01-01T01:01:01");
+  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+  private LocalDateTime lastMovedTime;
 
   @OneToOne
   Client client;
@@ -26,5 +32,10 @@ public class LastMovedTime {
   public LastMovedTime(LocalDateTime lastMovedTime, Client client) {
     this.lastMovedTime = lastMovedTime;
     this.client= client;
+  }
+
+  public void updateLastMovedTime(Client client, LastMovedTimeDto lastMovedTimeDto){
+    this.client=client;
+    this.lastMovedTime = lastMovedTimeDto.getLastMovedTime();
   }
 }
