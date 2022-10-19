68f50592e2650c90066c4a176f753f322b54ce8f
이건회 <rg970604@naver.com>
2022-10-20 00:18:02 +0900

    feat : LastMovedTimeDto.java

---
+++ backend/src/main/java/SADYz/backend/client/dto/LastMovedTimeDto.java
@@ -0,0 +1,26 @@
+package SADYz.backend.client.dto;
+
+import SADYz.backend.client.domain.Client;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+public class LastMovedTimeDto {
+
+  private Long id;
+
+  @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
+  private LocalDateTime lastMovedTime;
+
+  private Client client;
+
+}
