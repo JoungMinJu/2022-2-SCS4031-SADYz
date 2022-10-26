e7d50a9a2fb012ba1c1edd8863ce6049dd3e5f69
이건회 <rg970604@naver.com>
2022-10-27 01:49:56 +0900

    feat : DoorClosedTimeRepository.java

---
+++ backend/src/main/java/SADYz/backend/client/repository/DoorClosedTimeRepository.java
@@ -0,0 +1,9 @@
+package SADYz.backend.client.repository;
+
+import SADYz.backend.client.domain.Client;
+import SADYz.backend.client.domain.DoorClosedTime;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoorClosedTimeRepository extends JpaRepository<DoorClosedTime, Long> {
+  DoorClosedTime findByClient(Client client);
+}
