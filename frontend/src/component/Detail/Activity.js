8d1f5ed820c3dbe60d41843efc5fd02cb53c0712
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-23 03:06:55 +0900

    #29 feat: 노인 상세페이지 기본 셋팅

---
+++ frontend/src/component/Detail/Activity.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Activity(props) {
+  return <div></div>;
+}
+
+export default Activity;
--- frontend/src/routes/Detail.js
+++ frontend/src/routes/Detail.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import Activity from '../component/Detail/Activity';
+import BasicInfo from '../component/Detail/BasicInfo';
+import Chatbot from '../component/Detail/Chatbot';
+import DetailEmergency from '../component/Detail/Detail_Emergency';
 import Navbar from '../component/Navbar';
 import '../css/grid.css';
 
@@ -11,16 +15,25 @@ function Detail(props) {
         {`#mainpage {
                 filter: invert(11%) sepia(51%) saturate(890%) hue-rotate(333deg) brightness(100%) contrast(98%);`}
       </style>
-
       <div className="container">
+        <h2>관리 대상 노인 가구 정보</h2>
         <Container className="detail_container">
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
-          <div className="item"></div>
+          <div className="item title">기본 인적사항</div>
+          <div className="item">
+            <BasicInfo />
+          </div>
+          <div className="item title">챗봇</div>
+          <div className="item">
+            <Chatbot />
+          </div>
+          <div className="item title">응급콜</div>
+          <div className="item">
+            <DetailEmergency />
+          </div>
+          <div className="item title">활동정보</div>
+          <div className="item">
+            <Activity />
+          </div>
         </Container>
       </div>
     </>
@@ -33,5 +46,5 @@ const Container = styled.div`
   background: #ffffff;
   border-radius: 20px;
   width: 100%;
-  height: 100%;
+  height: 85%;
 `;
