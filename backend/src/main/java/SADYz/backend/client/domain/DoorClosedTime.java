53c3406082811ed8b6590b0f91d4870abf185aed
이건회 <rg970604@naver.com>
2022-11-24 05:45:28 +0900

    refactor(DoorClosedTime.java)

--- backend/src/main/java/SADYz/backend/client/domain/DoorClosedTime.java
+++ backend/src/main/java/SADYz/backend/client/domain/DoorClosedTime.java
@@ -3,11 +3,14 @@ package SADYz.backend.client.domain;
 import SADYz.backend.client.dto.DoorClosedTimeDto;
 import SADYz.backend.client.dto.LastMovedTimeDto;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import java.time.LocalDateTime;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import lombok.Builder;
 import lombok.Getter;
@@ -21,17 +24,19 @@ public class DoorClosedTime {
   @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  private String loginId;
+  private String phoneNumber;
   @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
   @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
   private LocalDateTime doorClosedTime;
   private boolean isOut;
   @OneToOne
+  @JoinColumn(name = "client_id")
+  @JsonIgnore
   Client client;
 
   @Builder
-  public DoorClosedTime(String loginId, LocalDateTime doorClosedTime, boolean isOut, Client client) {
-    this.loginId = loginId;
+  public DoorClosedTime(String phoneNumber, LocalDateTime doorClosedTime, boolean isOut, Client client) {
+    this.phoneNumber = phoneNumber;
     this.doorClosedTime = doorClosedTime;
     this.isOut = isOut;
     this.client = client;
