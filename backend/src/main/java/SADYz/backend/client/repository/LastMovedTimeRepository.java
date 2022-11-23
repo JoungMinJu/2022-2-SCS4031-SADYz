6bf7eddac7d957a5dfbc4a8d58c3d16940fdc041
이건회 <rg970604@naver.com>
2022-11-24 05:46:22 +0900

    refactor(LastMovedTimeRepository.java)

--- backend/src/main/java/SADYz/backend/client/repository/LastMovedTimeRepository.java
+++ backend/src/main/java/SADYz/backend/client/repository/LastMovedTimeRepository.java
@@ -2,6 +2,7 @@ package SADYz.backend.client.repository;
 
 import SADYz.backend.client.domain.Client;
 import SADYz.backend.client.domain.LastMovedTime;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LastMovedTimeRepository extends JpaRepository<LastMovedTime,Long> {
