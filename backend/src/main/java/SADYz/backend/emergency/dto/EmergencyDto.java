5601b353cdc743aafe107dea3c6f4e73e91229b2
이건회 <rg970604@naver.com>
2022-11-25 17:29:03 +0900

    refactor(LastMovedTimeRepository.java)

---
+++ backend/src/main/java/SADYz/backend/emergency/dto/EmergencyDto.java
@@ -0,0 +1,36 @@
+package SADYz.backend.emergency.dto;
+
+import SADYz.backend.client.domain.Client;
+import SADYz.backend.emergency.domain.Emergency;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EmergencyDto {
+
+    private Long id;
+    private boolean emergencyNow;
+    private Client client;
+
+    public static Emergency toEntity(EmergencyDto emergencyDto){
+        return Emergency.builder()
+            .id(emergencyDto.id)
+            .emergencyNow(emergencyDto.isEmergencyNow())
+            .client(emergencyDto.client)
+            .build();
+    }
+
+    public static EmergencyDto toDto(Emergency emergency){
+        return EmergencyDto.builder()
+            .id(emergency.getId())
+            .emergencyNow(emergency.isEmergencyNow())
+            .client(emergency.getClient())
+            .build();
+    }
+
+}
---
+++ backend/src/main/java/SADYz/backend/emergency/repository/EmergencyRepository.java
@@ -0,0 +1,12 @@
+package SADYz.backend.emergency.repository;
+
+import SADYz.backend.client.domain.Client;
+import SADYz.backend.emergency.domain.Emergency;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmergencyRepository extends JpaRepository<Emergency,Long> {
+    Emergency findByClient(Client client);
+    List<Emergency> findByEmergencyNow(boolean emergencyNow);
+
+}
