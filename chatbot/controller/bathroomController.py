47ffd8c36f1c812245aa816b4967f5008864daa3
MINJU <alswn9938@gmail.com>
2022-11-28 20:18:12 +0900

    modify : 응답 로직 변경

--- chatbot/controller/bathroomController.py
+++ chatbot/controller/bathroomController.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify
 
 from service import responseService, dbService
-from util import response
+from util.response import *
 
 bathroom = Blueprint("bathroom", __name__, url_prefix="/bathroom")
 
@@ -11,15 +11,15 @@ def start_bathroom():
     phone_number = request.values.get("phone_number")
     stay = dbService.is_client_at_home(phone_number)
     if not stay:
-        return response.NOT_IN_THE_HOUSE
+        return return_answer(NOT_IN_THE_HOUSE, DT_END) # -> -1
     # 화장실 이용 횟수 파악
     bathroom_count = dbService.get_today_bathroom_count(phone_number)
     print(bathroom_count)
     if bathroom_count < 4 :
-        return response.TOO_FEW_VISITS_BATHROOM # 상태 값 추가
+        return return_answer(TOO_FEW_VISITS_BATHROOM, DT_BATHROOM) # 상태 값 추가
     if bathroom_count > 8 :
-        return response.TOO_MUCH_VISITS_BATHROOM # 상태 값 추가
-    return response.NO_BATHROOM_PROBLEM
+        return return_answer(TOO_MUCH_VISITS_BATHROOM, DT_BATHROOM_HEALTH) # 상태 값 추가
+    return return_answer(NO_BATHROOM_PROBLEM, DT_END)
 
 @bathroom.post("/chat")
 def bathroom_chat():
@@ -27,23 +27,24 @@ def bathroom_chat():
     phone_number = request.values.get("phone_number")
     dialog_type = int(request.values.get("dialog_type"))
     input_list = input.split(" ")
-    if dialog_type == 1 :
+    if dialog_type == 3 :
         status = responseService.check_constipation_status(input_list)
 
         if status == "OK" :
-            return response.STATUS_IS_OK
+            return return_answer(STATUS_IS_OK, DT_END_SPEAK)
         if status == "ERROR":
-            return response.NOT_UNDERSTAND_CONSTIPATION
+            return return_answer(NOT_UNDERSTAND, DT_BATHROOM)
         if status == "NOT_OK":
-            return response.PROBLEM_IN_CONSTIPATION # 상태 값 추가
-    elif dialog_type == 2 :
+            return return_answer(PROBLEM_IN_CONSTIPATION, DT_END) # 상태 값 추가
+
+    elif dialog_type == 4 :
         status = responseService.check_health_status(input_list)
 
         if status == "OK" :
-            return response.STATUS_IS_OK
+            return return_answer(STATUS_IS_OK, DT_END_SPEAK)
         if status == "ERROR":
-            return response.NOT_UNDERSTAND_HEALTH
+            return return_answer(NOT_UNDERSTAND, DT_BATHROOM_HEALTH)
         if status == "NOT_OK":
-            return response.PROBLEM_IN_HEALTH # 상태 값 추가
+            return return_answer(PROBLEM_IN_HEALTH, DT_END_SPEAK) # 상태 값 추가
 
 
--- chatbot/controller/emotionController.py
+++ chatbot/controller/emotionController.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request
 from service import koelectra_qa, kogptchat
 from service import responseService, dbService, emotion_classification
-from util import response
+from util.response import *
 
 emotion = Blueprint("emotion", __name__, url_prefix="/emotion")
 
@@ -12,10 +12,10 @@ def start_emotion():
     phone_number = request.values.get("phone_number")
     stay = dbService.is_client_at_home(phone_number)
     if not stay:
-        return response.NOT_IN_THE_HOUSE
+        return return_answer(NOT_IN_THE_HOUSE, DT_END)
     question = responseService.get_call_to_say_hi()
     emotions.clear()
-    return response.call_to_say_hi(question)  # 응답 시간 기록하기 -> 정상 비정상 여부 
+    return return_answer(question, DT_EMOTION)  # 응답 시간 기록하기 -> 정상 비정상 여부
     # -> 배치로 n분 동안 대답 x인 사람 움직임 X인 사람 알려주기
 
 
@@ -25,8 +25,7 @@ def chat_emotion():
     input_split = input.split(" ")
     quit_answer = responseService.is_quit_answer(input_split)
     if quit_answer:
-        return response.QUIT  # 종료 -> 감정값 전달
-
+        return return_answer(QUIT, DT_END)  # 종료 -> 감정값 전달
     phone_number = request.values.get("phone_number")
     # 긍정 부정 판단
     classification = emotion_classification.predict(input)
@@ -37,4 +36,4 @@ def chat_emotion():
         answer = koelectra_qa.get_answer(input) 
     else:
         answer = kogptchat.predict(input)
-    return response.return_answer(answer)  # 종료 -> 응답 했다고 갱신
+    return return_answer(answer + IS_QUIT, DT_EMOTION) # 종료 -> 응답 했다고 갱신
