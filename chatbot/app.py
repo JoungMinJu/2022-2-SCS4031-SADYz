b05be640e8ec2ad0f2db2d332b201f75cd0aeecc
MINJU <alswn9938@gmail.com>
2022-11-30 19:53:01 +0900

    delete : 필요없는 파일 삭제

--- chatbot/app.py
+++ chatbot/app.py
@@ -20,4 +20,4 @@ if __name__ == '__main__':
     app.register_blueprint(meal)
     app.register_blueprint(bathroom)
     app.register_blueprint(emotion)
-    app.run(host='0.0.0.0', debug = True)
+    app.run(host='0.0.0.0')
