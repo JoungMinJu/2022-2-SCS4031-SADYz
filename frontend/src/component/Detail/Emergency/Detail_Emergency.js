7c946978eb10798744bb4a247c46e3613c811157
yaena1223 <yaena1223@dgu.ac.kr>
2022-12-03 16:03:54 +0900

    feat: detail 페이지 응급콜 카테고리 분류(미해결/전체)

--- frontend/src/component/Detail/Emergency/Detail_Emergency.js
+++ frontend/src/component/Detail/Emergency/Detail_Emergency.js
@@ -18,12 +18,27 @@ function DetailEmergency({ phonenumber }) {
   };
 
   useEffect(() => {
-    setInterval(() => {
-      get_emergency();
-    }, 1000);
-  }, []);
+    get_emergency();
+  }, [emergency]);
+
+  const selectList = ['미해결 응급콜', '전체 응급콜'];
+  const [Selected, setSelected] = useState('미해결 응급콜');
+  const handleSelect = (e) => {
+    setSelected(e.target.value);
+  };
+  useEffect(() => {
+    // console.log(Selected);
+  }, [Selected]);
+
   return (
     <div>
+      <Select onChange={handleSelect} value={Selected}>
+        {selectList.map((item) => (
+          <option value={item} key={item}>
+            {item}
+          </option>
+        ))}
+      </Select>
       <Table>
         <Thead>
           <tr>
@@ -34,18 +49,35 @@ function DetailEmergency({ phonenumber }) {
           </tr>
         </Thead>
         <Tbody>
-          {emergency.map((emerg, index) => {
-            return (
-              <Row
-                key={emerg.id}
-                id={emerg.id}
-                index={index}
-                time={emerg.createdDateTime}
-                contents={emerg.content}
-                solve={emerg.emergencyNow}
-              />
-            );
-          })}
+          {Selected === '미해결 응급콜'
+            ? emergency
+                .filter((emerg) => {
+                  return emerg.emergencyNow === true;
+                })
+                .map((emerg, index) => {
+                  return (
+                    <Row
+                      key={emerg.id}
+                      id={emerg.id}
+                      index={index}
+                      time={emerg.createdDateTime}
+                      contents={emerg.content}
+                      solve={emerg.emergencyNow}
+                    />
+                  );
+                })
+            : emergency.map((emerg, index) => {
+                return (
+                  <Row
+                    key={emerg.id}
+                    id={emerg.id}
+                    index={index}
+                    time={emerg.createdDateTime}
+                    contents={emerg.content}
+                    solve={emerg.emergencyNow}
+                  />
+                );
+              })}
         </Tbody>
       </Table>
     </div>
@@ -69,7 +101,7 @@ const Table = styled.table`
   table-layout: fixed;
   margin-left: auto;
   margin-right: auto;
-  margin-top: 10px;
+  margin-bottom: 10px;
   overflow-y: scroll;
   max-height: 250px;
 `;
@@ -89,4 +121,17 @@ const Tbody = styled.tbody`
   width: 100%;
   font-size: 1.5vmin;
   text-align: center;
+`;
+
+const Select = styled.select`
+  background: #ffffff;
+  mix-blend-mode: normal;
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  width: 150px;
+  height: 44px;
+  font-family: 'nanum_b';
+  font-size: 1.6vmin;
+  padding: 0.4vmin;
+  margin: 10px;
 `;
